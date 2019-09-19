02688d9c3b69c497325b594c212526b415d15074
ARNAB ROY <rarnab021@gmail.com>
2019-09-19 20:19:28 +0530

    Added actions to currency controller

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -3,8 +3,18 @@ class CurrenciesController < ApplicationController
   end
 
   def search 
+    @currencies = Currency.where("LOWER(name) LIKE ?", "%#{params[:search]}%")
+    render json: { currencies: @currencies }
   end
 
   def calculate 
+    @currency = Currency.find( params[:id])
+    amount = params[:amount]
+    render json: {
+      currency: @currency,
+      current_price: @currency.current_price,
+      amount: amount,
+      value: @currency.calculate_value(amount)
+    }
   end
 end
